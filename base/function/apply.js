7d72d4138a7b50e2db6f035a5ef58ebd01393446
lila <lila@pingansec.com>
2019-02-12 15:31:46 +0800

    函数apply方法

---
+++ base/function/apply.js
@@ -0,0 +1,25 @@
+/*
+
+虽然在一个独立的函数调用中，根据是否是strict模式，
+this指向undefined或window，不过，我们还是可以控制this的指向的！
+
+要指定函数的this指向哪个对象，可以用函数本身的apply方法，
+它接收两个参数，第一个参数就是需要绑定的this变量，
+第二个参数是Array，表示函数本身的参数。
+
+*/
+
+
+function getAge() {
+    var y = new Date().getFullYear();
+    return y - this.birth;
+}
+
+var xiaowang = {
+    name : '小王',
+    birth : 1990,
+    age : getAge
+};
+
+console.log("xiaowang's age " + xiaowang.age());
+console.log("xiaowang's age " + getAge.apply(xiaowang, []));
